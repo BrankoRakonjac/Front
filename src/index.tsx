4c412b3f8f3f1249de97fc58aa013404f4f93596
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-28 00:46:24 +0200

    Add main menu items propery for dynamic creation

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import  'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
-import { MainMenu } from './components/MainMenu/MainMenu';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import { MainMenu ,MainMenuItem } from './components/MainMenu/MainMenu';
+
+const menuItems = [
+  new MainMenuItem("Home", "/"),
+  new MainMenuItem("About us","/page/about-us/"),
+  new MainMenuItem("Contact","/contact/"),
+  new MainMenuItem("Log in","/user/login/"),
+
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainMenu></MainMenu>
+    <MainMenu items = {menuItems} ></MainMenu>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
