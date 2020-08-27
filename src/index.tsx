a7507e89eddb8c443aff48155241755f615c6f84
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-27 23:57:02 +0200

    Create the main menu component - static

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,14 @@ import  'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import { MainMenu } from './components/MainMenu/MainMenu';
 
 ReactDOM.render(
   <React.StrictMode>
+    <MainMenu></MainMenu>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
