10747ba7721d44096b90a2a2b52bb30f73be34ee
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-29 20:58:31 +0200

    Load top level categories

--- src/index.tsx
+++ src/index.tsx
@@ -14,15 +14,14 @@ import Switch from 'react-bootstrap/esm/Switch';
 import { ContactPage } from './components/ContactPage/ContactPage';
 import  UserLoginPage  from './components/UserLoginPage/UserLoginPage';
 import CategoryPage from './components/CategoryPage/CategoryPage';
+import  {UserRegistrationPage} from './components/UserRegistrationPage/UserRegistrationPage';
 const menuItems = [
   new MainMenuItem("Home", "/"),
   new MainMenuItem("Contact","/contact/"),
   new MainMenuItem("Log in","/user/login/"),
+  new MainMenuItem("Register","/user/register/"),
 
   
-  new MainMenuItem("Cat 1","/category/1/"),
-  new MainMenuItem("Cat 7","/category/7/"),
-  new MainMenuItem("Cat 21","/category/21/"),
 
 
 ]
@@ -37,6 +36,7 @@ ReactDOM.render(
             <Route exact path="/" component={HomePage}/>
             <Route path="/contact" component={ContactPage}/>
             <Route path="/user/login" component={UserLoginPage}/>
+            <Route path="/user/register" component={ UserRegistrationPage }/>
             <Route path="/category/:cId" component={ CategoryPage }/>
         </Switch>
       </HashRouter>
